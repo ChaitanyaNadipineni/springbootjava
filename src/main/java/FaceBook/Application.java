d2b5134d418503fa8ade4289db84d17166734d78
chaitanyanadipineni <nadipinenichaitanya@gmail.com>
2017-11-10 17:05:39 -0500

    first commit

---
+++ src/main/java/FaceBook/Application.java
@@ -0,0 +1,16 @@
+package FaceBook;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+//spring.social.facebook.appId=747320082142883
+   //     spring.social.facebook.appSecret=0786ea56d798ac916566914cfe86332a
